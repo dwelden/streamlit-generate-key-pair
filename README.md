a5da47f2b8ad056f9e539295b305714230d0dbab
David Welden <dwoogle@gmail.com>
2024-07-09 09:43:54 -0500

    Move passphrase input out of sidebar. Save passphrase in session state.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Streamlit Generate Key Pair for Authentication
 
 Based on https://medium.com/snowflake/generating-private-and-public-keys-using-snowflake-snowpark-46ca9c518fc3
+and https://danflippo.medium.com/generating-private-and-public-keys-using-streamlit-in-snowflake-a4ea1b48d5a1
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Based on https://medium.com/snowflake/generating-private-and-public-keys-using-snowflake-snowpark-46ca9c518fc3
+# and https://danflippo.medium.com/generating-private-and-public-keys-using-streamlit-in-snowflake-a4ea1b48d5a1
 
 import streamlit as st
 from cryptography.hazmat.primitives import serialization as s
@@ -6,7 +7,14 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 import base64, secrets, string
 from zipfile import ZipFile
 
-default_passphrase_length = 20
+def generate_passphrase():
+    """ Generate random passphrase to encrypt PEM Private Key"""
+    default_passphrase_length = 20
+    characters = string.digits+string.ascii_letters
+    passphrase = ''.join(
+        (secrets.choice(characters) for i in range(default_passphrase_length))
+    )
+    return passphrase
 
 def generate_key_pair(passphrase):
     """ Generate key pair and return as dictionary """
@@ -16,9 +24,6 @@ def generate_key_pair(passphrase):
     pub = s.PublicFormat.SubjectPublicKeyInfo
 
     rsa_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-    if not passphrase:
-        characters = string.digits+string.ascii_letters
-        passphrase = ''.join((secrets.choice(characters) for i in range(20)))
 
     file_names = (
         "encrypted_pem_private_key.pem",
@@ -52,11 +57,18 @@ def zip_for_download(keypair_dict):
 
     return zip_name
 
+if "passphrase" not in st.session_state:
+    st.session_state.passphrase = generate_passphrase()
+
 st.title(f"Generate Key Pair")
 st.image(image="images/keypair.jpeg")
+st.text("You can use the randomly generated passphrase or supply your own")
+st.session_state.passphrase = st.text_input(
+    label=f"Enter Passphrase for Encrypted PEM Private Key",
+    value=st.session_state.passphrase
+)
 
-passphrase = st.sidebar.text_input(f"Enter Passphrase (If left blank, a random {default_passphrase_length} character passphrase will be generated)")
-keypair_dict = generate_key_pair(passphrase)
+keypair_dict = generate_key_pair(st.session_state.passphrase)
 zip_name = zip_for_download(keypair_dict)
 
 with open(zip_name, 'rb') as z:
